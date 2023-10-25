52e0e4376727c876633573b4d838889641a9a8b3
109annas <abbyybugs@gmail.com>
2023-10-25 23:19:11 +0300

    New change

--- Mykod.cpp
+++ Mykod.cpp
@@ -8,18 +8,19 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	float Z, dobchik, y;
+	float Z, Dobchik, y;
 	cout << "Ââåä³òü ó\ny=";
 	cin >> y;
-	dobchik = 1;
+	Dobchik = 1;
 	if (pow(y, 2) + 5 == 0) {
 		cout << "ÏÎÌÈËÊÀ";
 	}
 	else {
 		for (int n = 1; n <= 10; n++) {
-			dobchik *= n;
+			Dobchik *= n;
 		}
-		Z = (y + dobchik + 2) / (pow(y, 2) + 5);
+		Z = (y + Dobchik + 2) / (pow(y, 2) + 5);
 		cout << "Z=" << Z;
 	}
 }
+//hello
