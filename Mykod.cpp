cfe87c1dd72b9ec394809f2884a3a4aec60b788a
109annas <abbyybugs@gmail.com>
2023-11-01 19:59:16 +0200

    add new tag

--- Mykod.cpp
+++ Mykod.cpp
@@ -21,7 +21,19 @@ int main()
 		}
 		Z = (y + Dobchik + 2) / (pow(y, 2) + 5);
 		cout << "Z=" << Z;
-		cout << "Кінець";
+		cout << "Кінець!!!";
+		float x, Y, Z, b;
+		int a;
+		printf("Введи Х: ");
+		scanf_s("%f", &x);
+		printf("Введи У: ");
+		scanf_s("%f", &Y);
+		printf("Введи Z: ");
+		scanf_s("%f", &Z);
+		b = Y - x;
+		a = static_cast<int> ((((fabs(Y - Z)) / b) / (1 + pow(b, 3))) + pow(10 * Y - 17, 1.0 / 3) - pow(x, 3) - pow(Y, 3 - x));
+		printf("\nРезультат прикладу: \na=%i\n", a);
+
 	}
 }
 //hello
